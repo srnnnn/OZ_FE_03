b4d8ba41e6c7dd6314fb4c482c40efc012655e3c
srnnnn <tpfla311@naver.com>
2024-05-10 15:08:52 +0900

    0510 react 4일차 과제

--- oz-react-study/01_react-crud-app/src/components/ExpenseItem.js
+++ oz-react-study/01_react-crud-app/src/components/ExpenseItem.js
@@ -3,18 +3,18 @@ import './ExpenseItem.css'
 import {MdEdit, MdDelete} from "react-icons/md";
 
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({expense, handleEdit, handleDelete}) => {
   return (
     <li className='item'>
     <div className='info'>
-        <span className='expense'>{props.expense.charge}</span>
-        <span className='amount'>{props.expense.amount}</span>
+        <span className='expense'>{expense.charge}</span>
+        <span className='amount'>{expense.amount}</span>
     </div>  
     <div>
-        <button className='edit-btn'>
+        <button className='edit-btn' onClick={()=> handleEdit(expense.id)}>
             <MdEdit/>
         </button>
-        <button className='clear-btn' onClick={()=>props.handleDelete(props.expense.id)}>
+        <button className='clear-btn' onClick={()=>handleDelete(expense.id)}>
             <MdDelete/>
         </button>
     </div>
