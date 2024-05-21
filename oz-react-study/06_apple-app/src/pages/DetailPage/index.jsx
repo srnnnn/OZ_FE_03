4c9ff120c0fe5c7a38a512ef554c8aff6daf7c9f
srnnnn <tpfla311@naver.com>
2024-05-21 16:26:06 +0900

    0521 commit

--- oz-react-study/06_apple-app/src/pages/DetailPage/index.jsx
+++ oz-react-study/06_apple-app/src/pages/DetailPage/index.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import axios from '../../api/axios';
 import { imageBasePath } from "../../constant";
+import { styled } from 'styled-components';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
 
 const DetailPage = () => {
 
@@ -20,14 +25,80 @@ const DetailPage = () => {
 
   if (!movie) return null;
 
+  console.log(movie)
+
   return (
-    <section>
-      <img
-        src={`${imageBasePath}${movie.backdrop_path}`}
-        alt="detail"
-      />
-    </section>
+    <Container>
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      >
+      <SwiperSlide>
+        <Wrap>
+        <img
+          src={`${imageBasePath}${movie.poster_path
+          }`}
+          alt="detail"
+        />
+        </Wrap>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Wrap>
+        <img
+          src={`${imageBasePath}${movie.backdrop_path}`}
+          alt="detail"
+        />
+        </Wrap>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Wrap>
+        {movie.title}
+        </Wrap>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Wrap>
+          
+          
+        </Wrap>
+      </SwiperSlide>
+      </Swiper>
+
+    </Container>
   )
 }
 
+const Container = styled.div`
+  padding: 0 0 26px;
+`
+const Wrap = styled.div`
+margin-top:70px;
+width: 100%;
+height: 500px;
+border-radius: 10px;
+cursor: pointer;
+overflow: hidden;
+position: relative;
+transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+border: 3px solid rgba(249, 249, 249, 0.1);
+img {
+  inset: 0px;
+  display: block;
+  height: 100%;
+  object-fit: cover;
+  opacity: 1;
+  position: absolute;
+  transition: opacity 500ms ease-in-out 0s;
+  width: 100%;
+  z-index: 1;
+  top: 0;
+}
+&:hover {
+  box-shadow: rgb(0 0 0 / 30%) 0px 0px 28px 0px,
+    rgb(0 0 0 / 72%) 0px 10px 12px -10px;
+  transform: scale(0.98);
+  border-color: rgba(249, 249, 249, 0.8);
+}
+`
 export default DetailPage
