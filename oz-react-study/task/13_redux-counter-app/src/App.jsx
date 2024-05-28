3c1229ed476eccb9269879844f0e8801922c8e5b
srnnnn <tpfla311@naver.com>
2024-05-28 13:24:16 +0900

    commit

---
+++ oz-react-study/task/13_redux-counter-app/src/App.jsx
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
+// import { increment, asyncIncrement } from './redux/action';
+import './App.css'
+
+function App() {
+  const counter = useSelector((state) => state);
+   console.log(counter)
+
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({type:'INCREMENT'})
+ }
+  const handleDecrement = () => {
+    dispatch({type:'DECREMENT'}) 
+ }
+ const handleAsyncIncrement = () => {
+  setTimeout(() => {
+    dispatch({ type: 'INCREMENT' });
+  }, 1000);
+ }
+
+  return (
+    <div>
+      <h1>Count: {counter}</h1>
+      <button onClick={handleIncrement}>+ 1</button>
+      <button onClick={handleDecrement}>- 1</button>
+      <button onClick={handleAsyncIncrement}>1초 후 +1</button>
+    
+    </div>
+  );
+}
+
+export default App;
